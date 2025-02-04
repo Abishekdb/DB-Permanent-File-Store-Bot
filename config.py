2688fe24f1dae06e6acd4e90644b832c13373d4c
Abishekdb <dbtelegrambots@gmail.com>
2025-02-04 17:32:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
+URL = environ.get("URL", "eastern-goldfish-annadb-01849fa2.koyeb.app/")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
